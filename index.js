ad4ef90d007706ce236ae3150a3331744fc9cf01
ken025 <kennethmanuel250@gmail.com>
2020-11-17 18:38:27 -0500

    Done.

--- index.js
+++ index.js
@@ -11,3 +11,20 @@ function handleClick(e) {
 
 /* Write your implementation of greet() */
 /* Write your implementation of displayMessage() */
+
+
+function greet(time){
+  const timeAr = time.split(':');
+  const hour = parseInt(timeAr[0]);
+    if (hour < 12){
+      return "Good Morning"
+    } else if (hour > 17 ){
+      return "Good Evening"
+    } else {
+      return "Good Afternoon"
+    }
+}
+
+function displayMessage(message){
+   document.getElementById("greeting").innerText = message;
+ }
