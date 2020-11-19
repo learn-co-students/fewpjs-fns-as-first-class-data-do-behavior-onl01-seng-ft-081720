21c486214a0ccafb874c5408e798df9815e35652
ts-miller <tylerstephen11@gmail.com>
2020-11-19 16:14:42 -0500

    Done.

--- index.js
+++ index.js
@@ -10,4 +10,24 @@ function handleClick(e) {
 /* End Given Code, don't edit above here...*/
 
 /* Write your implementation of greet() */
+
+function greet(enteredTime) {
+
+  time = enteredTime.split(":")[0]
+
+  if (time < 12) {
+    return "Good Morning"
+  }
+  if (time > 12 && time < 17) {
+    return "Good Afternoon"
+  }
+  if (time > 17 ) {
+    return "Good Evening"
+  }
+}
 /* Write your implementation of displayMessage() */
+
+function displayMessage(msg) {
+  const greeting = document.querySelector('#greeting')
+  greeting.innerText = msg
+}
