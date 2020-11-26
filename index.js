3c4a21ce48b7a2ae44b049203295771aaf574c89
maxjacobzander <maxjacobzander@gmail.com>
2020-11-26 08:59:28 -0600

    Done.

--- index.js
+++ index.js
@@ -10,4 +10,13 @@ function handleClick(e) {
 /* End Given Code, don't edit above here...*/
 
 /* Write your implementation of greet() */
+function greet(timeString) {
+  let inputTime = parseInt(timeString, 10);
+  if (inputTime < 12) return "Good Morning"
+  if (inputTime > 17) return "Good Evening"
+  else return "Good Afternoon"
+}
 /* Write your implementation of displayMessage() */
+function displayMessage(message) {
+  document.getElementById('greeting').innerText = message;
+}
